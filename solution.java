d5ffdf45f12050baafb3411b0c07584f723cd310
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2017-12-21 16:43:25 +0800

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,10 @@
+# Reverse_integer
+package reverse_integer;
+
+public class reverse_integer {
+
+	public static void main(String[] args){
+	solution Solution=new solution();
+	Solution.reverse(153);
+}
+}
